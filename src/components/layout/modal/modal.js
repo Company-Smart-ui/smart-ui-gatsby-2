dc8fba89f6a6843248e8dad008936f2782ff3b96
skirilenko <kirilenko-sofia@smart-ui.pro>
2022-08-18 19:15:45 +0300

    ended modal

--- src/components/layout/modal/modal.js
+++ src/components/layout/modal/modal.js
@@ -1,42 +1,19 @@
 import React from 'react';
 import * as style from "./modal.module.scss";
-import {Messenger} from "../../../global/messengers/messengers.js";
+import {ModalForm} from "./modalForm/modalForm";
+import {useOpen} from "../../../hooks/useOpen";
+
+
 
 export const Modal = () => {
+    const {isOpen, onClose, onOpen}= useOpen(false);
+
     return <div className={style.wrapper}>
-        <button className={style.btn + " button"}>return message</button>
-        <div className={style.modal}>
-            <div className={style.header}>
-                <h3>Leave your message</h3>
-                <div  className={'md-only'}><Messenger /></div>
-                
-            </div>
-            <form className={style.form}>
-                <p>Please leave one of your contacts to contact you.</p>
-                <div className={style.field}>
-                    <input value='' placeholder='Leave any contacts: ( telegram ,whatsapp, linkedin , etc.. )' type='text'/>
-                    <fieldset>
-                        <legend>Leave any contacts:</legend>
-                    </fieldset>
-                </div>
+        <button className={style.btn + " button"} onClick={onOpen}>return message</button>
+        {
+            isOpen && <ModalForm  onClose={onClose} /> 
 
-                <div className={style.field}>
-                    <textarea value='' placeholder='Your message*'></textarea>
-                    <fieldset>
-                        <legend>'Your message*'</legend>
-                    </fieldset>
-                </div>
-
-                <p className='md-only'>Your data is safe and will not be passed on to third parties</p>
-
-                <div  className={'sm-only'}><Messenger /></div>
-
-                <div className={style.btns}>
-                    <button className='reset'>No, cancel</button>
-                    <button type='submit' className='button'>Yes, confirm</button>
-                </div>
-
-            </form>
-        </div>
+        }
+        
         </div>
 }
