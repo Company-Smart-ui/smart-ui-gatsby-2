3cf36517ac30a7e3b137af40e1cce59cc46202b2
Viktor <80700897+viktormoskalev07@users.noreply.github.com>
2022-08-26 17:19:45 +0300

    images strapi integration

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -4,11 +4,19 @@ import "../../global.scss"
 import "../../lazy.scss"
 import {Header} from "./header/header";
 import { Modal } from './modal/modal';
+import {navigate} from "gatsby";
 // import {RedLine} from "./redLine/redLine";
 
  const  Layout = ( props) => {
     const path = props?.children?.props?.pageContext?.pageName;
-     console.log(path)
+    if(path===""){
+        if(typeof window!=="undefined"){
+            navigate('/home');
+        }
+
+        return <> </>
+    }
+
     return (
         <div className={'layout'}>
             <Header path={ path }/>
