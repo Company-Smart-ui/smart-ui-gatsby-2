9519f72292c96efb2972f4a169c08ed9db62cefd
Viktor <80700897+viktormoskalev07@users.noreply.github.com>
2022-09-06 12:15:49 +0300

    refresh

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,80 +1,86 @@
-
 const strapiConfig = {
-    apiURL: 'https://stark-refuge-01750.herokuapp.com/' ,
-    accessToken:  '9a50ad95c89407920b468f977558d9a52e70c88b21eddeeb97b65c82b695ce42d9c0afa6a9623aa1b45ab31c6aa2592e173bcef5a3c883c35bb8818110eb7179c6dd94ac903aecc5323f847845f1d2c455a96d22225bfccde2f8aa469c45a66d78307af131e72a23f2d4cd65153cae7e5cbd65782ece1ac3e3ab3a9f625a3f27',
-    collectionTypes: [ {
+    apiURL: 'https://stark-refuge-01750.herokuapp.com/',
+    accessToken: '9a50ad95c89407920b468f977558d9a52e70c88b21eddeeb97b65c82b695ce42d9c0afa6a9623aa1b45ab31c6aa2592e173bcef5a3c883c35bb8818110eb7179c6dd94ac903aecc5323f847845f1d2c455a96d22225bfccde2f8aa469c45a66d78307af131e72a23f2d4cd65153cae7e5cbd65782ece1ac3e3ab3a9f625a3f27',
+    collectionTypes: [{
         singularName: 'single-project',
         queryParams: {
-            populate:  "deep"
+            populate: "deep"
         },
     },
     ],
-    singleTypes: [     {
+    singleTypes: [{
         singularName: 'home',
         queryParams: {
-            populate:  "deep"
+            populate: "deep"
         },
     },
         {
-        singularName: 'global',
-        queryParams: {
-            populate:  "deep"
+            singularName: 'global',
+            queryParams: {
+                populate: "deep"
+            },
         },
-    },
 
 
     ],
     queryLimit: 1000,
 };
-
+// const noTranslate = ['admin', 'login', 'dev-404-page', '404', '/:lang?/project/:uid', '/:lang?/cv/:uid']
+// const nt = noTranslate.map(n => {
+//     return {
+//         matchPath: n,
+//         languages: ['en']
+//     }
+// })
 
 module.exports = {
-  siteMetadata: {
-    title: `Smart-ui `,
-      description: `Smart ui  frontend  Agency -  React js , Gatsby , Next `,
-      image: `/logo-large.png`,
-    siteUrl: `https://smart-ui-gatsby-2.vercel.app/`
-  },
-  plugins: [
-
-      {
-          resolve: `gatsby-source-strapi`,
-          options: strapiConfig,
-      },
-   "gatsby-plugin-sass", "gatsby-plugin-image",  "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Smart-ui `,
+        description: `Smart ui  frontend  Agency -  React js , Gatsby , Next `,
+        image: `/logo-large.png`,
+        siteUrl: `https://smart-ui-gatsby-2.vercel.app/`
     },
-    __key: "images"
-  }
-  ,
-      // 'gatsby-plugin-minify-classnames',
-      {
-          resolve: `gatsby-plugin-alias-imports`,
-          options: {
-              alias: {
-                  "@src": "src",
-              },
-              extensions: [
-                  "js , scss",
-              ],
-          }
-      },
-      {
-          resolve: `gatsby-plugin-react-i18next`,
-          options: {
-              languages: [`en`, `uk-UA`],
-              defaultLanguage: `en`,
-              debug:true
-          }
-      }
+    plugins: [
 
-  ]
+        {
+            resolve: `gatsby-source-strapi`,
+            options: strapiConfig,
+        },
+        "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                "icon": "src/images/icon.png"
+            }
+        }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "images",
+                "path": "./src/images/"
+            },
+            __key: "images"
+        }
+        ,
+        // 'gatsby-plugin-minify-classnames',
+        {
+            resolve: `gatsby-plugin-alias-imports`,
+            options: {
+                alias: {
+                    "@src": "src",
+                },
+                extensions: [
+                    "js , scss",
+                ],
+            }
+        },
+        {
+            resolve: `gatsby-plugin-react-i18next`,
+            options: {
+                languages: [`en`, `uk-UA`],
+                defaultLanguage: `en`,
+                debug: true,
+                // pages: nt
+            }
+        }
+
+    ]
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,13 @@
 const axios = require("axios");
 const ENDPOINT = 'https://stark-refuge-01750.herokuapp.com/api/'
-const PAGES_API = ['home' ,'portfolio' , 'global'];
+const PAGES_API = ['home', 'portfolio', 'global'];
 const LANGUAGES = ['en', 'uk-UA'];
 const path = require(`path`)
 
-const createField = async ({lang} )=> {
-    const pages =[];
+const createField = async ({lang}) => {
+    const pages = [];
     for (let page of PAGES_API) {
-        const fetchData = () => axios.get(ENDPOINT +page + "?locale=" + lang+"&populate=deep");
+        const fetchData = () => axios.get(ENDPOINT + page + "?locale=" + lang + "&populate=deep");
         const field = await fetchData();
         pages.push({
             ns: page,
@@ -15,24 +15,24 @@ const createField = async ({lang} )=> {
             language: lang
         })
     }
- return pages
+    return pages
 }
-const everyLang =async ()=>{
-    let  everyLang =[]
+const everyLang = async () => {
+    let everyLang = []
     for (let lang of LANGUAGES) {
         const localedPages = await createField({lang})
 
-        everyLang = [...everyLang ,...localedPages]
+        everyLang = [...everyLang, ...localedPages]
     }
     return everyLang
 }
 
 
-exports.sourceNodes =async ({actions: {createNode}, createNodeId, createContentDigest}) => {
-  const everyPagesLang = await  everyLang();
+exports.sourceNodes = async ({actions: {createNode}, createNodeId, createContentDigest}) => {
+    const everyPagesLang = await everyLang();
     return everyPagesLang.map((page, i) => createNode({
         ...page,
-        id: createNodeId(page.ns+ i),
+        id: createNodeId(page.ns + i),
         internal: {
             type: `Locale`,
             contentDigest: createContentDigest(page)
@@ -41,21 +41,20 @@ exports.sourceNodes =async ({actions: {createNode}, createNodeId, createContentD
 };
 
 
-
-exports.onCreatePage = ({ page, actions }) => {
-    const { createPage, deletePage } = actions
+exports.onCreatePage = ({page, actions}) => {
+    const {createPage, deletePage} = actions
     deletePage(page)
     createPage({
         ...page,
         context: {
             ...page.context,
-            pageName:[page.context.i18n.originalPath.replace(/\//g, "" ) , "global"],
+            pageName: [page.context.i18n.originalPath.replace(/\//g, ""), "global"],
         },
     })
 }
 
-exports.createPages = async ({ actions, graphql }) => {
-    const { createPage } = actions
+exports.createPages = async ({actions, graphql}) => {
+    const {createPage} = actions
     const singlePortfolio = path.resolve(`src/templates/singlePortfolio/singlePortfolio.js`)
     const result = await graphql(`
     query {
@@ -70,13 +69,13 @@ exports.createPages = async ({ actions, graphql }) => {
     }
   `)
     result.data.allStrapiSingleProject.edges.forEach(edge => {
-            createPage({
-                path: `${edge.node.project_name.replace(/[ ,./!@#$%^&*(?)=:;'"]/g , '_')}`,
-                component: singlePortfolio,
-                context: {
-                   pageId:edge.node.id
-                },
-            })
+        createPage({
+            path: `project/${edge.node.project_name.replace(/[ ,./!@#$%^&*(?)=:;'"]/g, '_')}`,
+            component: singlePortfolio,
+            context: {
+                pageId: edge.node.id
+            },
         })
+    })
 
 }
