ca3c928c5fd8a52adb98e9a17d46b15cb389bb62
Viktor <80700897+viktormoskalev07@users.noreply.github.com>
2022-09-06 20:47:24 +0300

    test

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,19 +1,25 @@
+require("@fontsource/dm-sans")
+require("@fontsource/dm-sans/700.css")
+
+require("@fontsource/poppins")
+require("@fontsource/poppins/500.css")
+
 const React = require("react")
 const Layout = require("./src/components/layout/layout").default
 const P404 = require("./src/pages/404").default
 
 
-exports.wrapPageElement = ({ element }) => {
+exports.wrapPageElement = ({element}) => {
     const newElement = React.cloneElement(
-        element||  <P404/> ,  // I18nextProvider
+        <P404/>,  // I18nextProvider
         element.props,
         React.cloneElement(
-            element?.props?.children||  <P404/> ,  // I18nextContext.Provider
-            element?.props?.children?.props ,
+            <P404/>,  // I18nextContext.Provider
+            element?.props?.children?.props,
             React.createElement(
                 Layout,
                 undefined,
-                element?.props?.children?.props?.children||  <P404/> ,
+                <P404/>,
             ),
         ),
     );
