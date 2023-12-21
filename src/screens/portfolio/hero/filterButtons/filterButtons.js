6a3f3f70b0a639213fa9608e591297fe97cddec2
webTime <webTime@gmail.com>
2023-12-21 16:52:58 +0200

    corrected filter portfolio

--- src/screens/portfolio/hero/filterButtons/filterButtons.js
+++ src/screens/portfolio/hero/filterButtons/filterButtons.js
@@ -4,11 +4,13 @@ import * as style from "./filterButtons.module.scss";
 const filterOptions = [
   { title: "All", link: "/" },
   { title: "Angular", link: "angular" },
-  { title: "Gatsby", link: "gatsby" },
-  { title: "HTML", link: "html" },
+  // { title: "Gatsby", link: "gatsby" },
+  // { title: "HTML", link: "html" },
   { title: "React", link: "react" },
   { title: "Next.Js", link: "next.js" },
   { title: "Wordpress", link: "wordpress" },
+  { title: "UI/UX", link: "UI/UX" },
+  { title: "Django.REST", link: "django.rest" },
 ];
 
 export const FilterButtons = ({ query, filterListHandler }) => (
